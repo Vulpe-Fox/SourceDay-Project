9bcbf087eb539f9ef9dcd1003da52c5fdda673a0
Vulpe-Fox <lagopus39@gmail.com>
2026-03-23 16:21:58 -0400

    Added spec to views to test different system states

---
+++ spec/views/pages/home.html.erb_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe "pages/home", type: :view do
+  let(:user) { create(:user) }
+
+  before do
+    without_partial_double_verification do
+      allow(view).to receive(:current_user).and_return(current_user)
+    end
+  end
+
+  context "when no user is logged in" do
+    let(:current_user) { nil }
+
+    it "prompts the user to use Dev Login" do
+      render
+      expect(rendered).to have_content("Please use the Dev Login")
+      expect(rendered).not_to have_link("Connect to Todoist")
+      expect(rendered).not_to have_selector(".card")
+    end
+  end
+
+  context "when user is logged in but not linked" do
+    let(:current_user) { user }
+
+    it "shows the red Connect to Todoist buttons" do
+      render
+      expect(rendered).to have_link("Connect to Todoist", href: todoist_connect_path)
+      expect(rendered).to have_content("Status: Not Connected")
+      expect(rendered).not_to have_link("View My Tasks")
+    end
+  end
+
+  context "when user is logged in and linked" do
+    let(:current_user) { create(:user, :with_todoist) }
+
+    it "shows success message and task link" do
+      render
+      expect(rendered).to have_content("Your Todoist account is successfully connected!")
+      expect(rendered).to have_link("View My Tasks", href: tasks_path)
+      
+      expect(rendered).to have_content("Status: Connected")
+      expect(rendered).to have_link("Reconnect Todoist", href: todoist_connect_path)
+    end
+  end
+end
---
+++ spec/views/tasks/index.html.erb_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe "tasks/index", type: :view do
+  let(:tasks_collection) do
+    [
+      { 
+        "content" => "Buy Groceries", 
+        "priority" => 4, 
+        "due" => { "string" => "2026-03-24" } 
+      },
+      { 
+        "content" => "Call Mom", 
+        "priority" => 1, 
+        "due" => nil 
+      }
+    ]
+  end
+
+  context "when tasks are present" do
+    before do
+      assign(:tasks, tasks_collection)
+      render
+    end
+
+    it "renders a list of tasks" do
+      expect(rendered).to have_content("Buy Groceries")
+      expect(rendered).to have_content("2026-03-24")
+      expect(rendered).to have_content("P4")
+      
+      expect(rendered).to have_content("Call Mom")
+      expect(rendered).to have_content("No date")
+      expect(rendered).to have_content("P1")
+    end
+
+    it "renders the 'Create New Task' form" do
+      expect(rendered).to have_selector("form[action='#{tasks_path}'][method='post']")
+      expect(rendered).to have_field("Task Name")
+      expect(rendered).to have_select("Priority")
+      expect(rendered).to have_button("Add Task")
+    end
+  end
+
+  context "when no tasks are found" do
+    before do
+      assign(:tasks, [])
+      render
+    end
+
+    it "shows a placeholder message" do
+      expect(rendered).to have_content("No tasks found.")
+    end
+  end
+end
