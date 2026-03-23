f3a908877cbed162ad91d896bdb3d813350be5b6
Vulpe-Fox <lagopus39@gmail.com>
2026-03-23 16:20:32 -0400

    Updated user model to increase security of passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,13 @@ class User < ApplicationRecord
   encrypts :todoist_access_token
 
   # Validations
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, length: { minimum: 8 }, allow_nil: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 8 },
+            format: {
+              with: /\A(?=.*[A-Z])(?=.*[^A-Za-z0-9]).+\z/,
+              message: I18n.t("sessions.signup.password_invalid")
+            },
+            allow_nil: true
 
   # check if user is connected to todoist
   def todoist_linked?
